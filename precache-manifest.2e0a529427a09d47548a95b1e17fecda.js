61b41ec14ef8da2af6a93877c8f46f4966431e05
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2020-09-24 13:36:25 +0200

    Updates

---
+++ precache-manifest.2e0a529427a09d47548a95b1e17fecda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc877515a8b741bffbc8e6c8f24edf36",
+    "url": "/github-page/index.html"
+  },
+  {
+    "revision": "49b1fdc5e2860d7c35f5",
+    "url": "/github-page/static/css/main.47e10bcd.chunk.css"
+  },
+  {
+    "revision": "acab01d97257db9ecd4c",
+    "url": "/github-page/static/js/2.b5a9f54c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-page/static/js/2.b5a9f54c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b1fdc5e2860d7c35f5",
+    "url": "/github-page/static/js/main.2f5f4fbe.chunk.js"
+  },
+  {
+    "revision": "634cff5e07bd19d7a0fd",
+    "url": "/github-page/static/js/runtime-main.3d81935f.js"
+  },
+  {
+    "revision": "e1440a6fdc36a87637b3421c783f3933",
+    "url": "/github-page/static/media/ko-fi.e1440a6f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
